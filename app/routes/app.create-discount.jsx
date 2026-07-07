cd61b25f4303ad5c160dd98f60b91ad50c3d42cb
Mohamed-Saad288 <mohamed.saadd288@gmail.com>
2026-07-07 22:10:15 +0300

    Fix hydration error in create-discount page

--- app/routes/app.create-discount.jsx
+++ app/routes/app.create-discount.jsx
@@ -1,11 +1,10 @@
-import { data, useActionData } from "react-router";
+import { data, useActionData, useSubmit, useNavigation } from "react-router";
 import { authenticate } from "../shopify.server";
 
 export const action = async ({ request }) => {
   const { admin } = await authenticate.admin(request);
-
   try {
-console.log("CREATING DISCOUNT...");
+    console.log("CREATING DISCOUNT...");
     const response = await admin.graphql(
       `#graphql
       mutation CreateAutomaticDiscount(
@@ -19,7 +18,6 @@ console.log("CREATING DISCOUNT...");
             title
             status
           }
-
           userErrors {
             field
             message
@@ -30,12 +28,8 @@ console.log("CREATING DISCOUNT...");
         variables: {
           automaticAppDiscount: {
             title: "Bag Offer",
-
-            functionId:
-              "019f3840-3b45-71bc-90b9-6452a0514297",
-
+            functionId: "019f3840-3b45-71bc-90b9-6452a0514297",
             startsAt: new Date().toISOString(),
-
             combinesWith: {
               productDiscounts: true,
               orderDiscounts: false,
@@ -45,26 +39,15 @@ console.log("CREATING DISCOUNT...");
         },
       }
     );
-
     const result = await response.json();
+    console.log("CREATE DISCOUNT RESULT:", JSON.stringify(result, null, 2));
 
-    console.log(
-      "CREATE DISCOUNT RESULT:",
-      JSON.stringify(result, null, 2)
-    );
-console.log("DISCOUNT RESPONSE:", JSON.stringify(result));
     return data({
       success: true,
       result,
     });
-
   } catch (error) {
-
-    console.error(
-      "CREATE DISCOUNT ERROR:",
-      error
-    );
-
+    console.error("CREATE DISCOUNT ERROR:", error);
     return data(
       {
         success: false,
@@ -77,33 +60,29 @@ console.log("DISCOUNT RESPONSE:", JSON.stringify(result));
   }
 };
 
-
 export default function CreateDiscount() {
-
   const result = useActionData();
+  const submit = useSubmit();
+  const navigation = useNavigation();
+  const isSubmitting = navigation.state === "submitting";
+
+  const handleCreate = () => {
+    submit({}, { method: "post" });
+  };
 
   return (
     <s-page heading="Create Bag Offer">
-
       <s-section>
-
-        <form method="post">
-  <s-button type="submit">
-    Create Discount
-  </s-button>
-</form>
-
-
-        {result && (
-          <s-section>
-            <pre>
-              {JSON.stringify(result, null, 2)}
-            </pre>
-          </s-section>
-        )}
-
+        <s-button onClick={handleCreate} disabled={isSubmitting}>
+          {isSubmitting ? "Creating..." : "Create Discount"}
+        </s-button>
       </s-section>
 
+      {result && (
+        <s-section>
+          <pre>{JSON.stringify(result, null, 2)}</pre>
+        </s-section>
+      )}
     </s-page>
   );
 }
